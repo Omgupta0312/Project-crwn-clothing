6012f5bc23de2bcac4be7143359b8208e63d7bc6
OM GUPTA <omgupta0312@gmail.com>
2022-06-24 00:33:13 +0530

    React Route basic feature added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import Directory from "./component/Directory/Directory.component.jsx"
-import categories from "./component/category-item/cateogries";
+import { Routes, Route ,Outlet} from "react-router-dom"
+import Home from "./routes/home/home.component";
+const Navigation=()=>{
+  return (
+    <div>
+        <h1>This is Navigation bar</h1>
+        <Outlet />
+    </div>
+  );
+};
+
+const Shop1 = () => (
+   <h1> This is ./home/shop1 </h1>
+);
 
 
 const App = () => {
   return (
-    <Directory
-      categories={categories}
-    />
-  )
+    <Routes>
+      <Route path="/" element={<Navigation/>}>
+        <Route index element={<Home />} />
+        <Route path="shop1" element={<Shop1 />} />
+        
+      </Route>
+    </Routes>
 
+  );
 }
-
 export default App;
